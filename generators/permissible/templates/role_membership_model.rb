ded14156ebb257ce319fbdc2dcbe19f22f0c8854
Noam Ben-Ari <nbenari@gmail.com>
2008-05-10 16:49:43 +0300

    fixed loop membership issue in RoleMembership validations with specs.

--- generators/permissible/templates/role_membership_model.rb
+++ generators/permissible/templates/role_membership_model.rb
@@ -7,21 +7,30 @@ class <%= role_membership_model_name %> < ActiveRecord::Base
   validates_uniqueness_of :<%= role_model_file_name %>_id, :scope => [:roleable_id, :roleable_type]
   validates_numericality_of :roleable_id, :<%= role_model_file_name %>_id
   validates_format_of :roleable_type, :with => /^[A-Z]{1}[a-z0-9]+([A-Z]{1}[a-z0-9]+)*$/
-  validate :<%= role_model_file_name %>_does_not_belong_to_itself
-  validate :<%= role_model_file_name.pluralize %>_cannot_belong_to_each_other
+  validate :<%= role_model_file_name %>_does_not_belong_to_itself_in_a_loop
   
   protected
-  def <%= role_model_file_name %>_does_not_belong_to_itself
-    if <%= role_model_file_name %>_id == roleable_id && roleable_type == "<%= role_model_name %>"
-      errors.add_to_base("A <%= role_model_file_name %> cannot belong to itself.")
+  def <%= role_model_file_name %>_does_not_belong_to_itself_in_a_loop
+    if roleable_type == "<%= role_model_name %>"
+      if <%= role_model_file_name %>_id == roleable_id
+        errors.add_to_base("A <%= role_model_file_name %> cannot belong to itself.")
+      else
+        if belongs_to_itself_through_other?(roleable_id, <%= role_model_file_name %>_id)
+          errors.add_to_base("A <%= role_model_file_name %> cannot belong to a <%= role_model_file_name %> which belongs to it.")
+        end
+      end
     end
   end
   
-  def <%= role_model_file_name.pluralize %>_cannot_belong_to_each_other
-    if roleable_type == '<%= role_model_name %>'
-      if self.class.find(:first, :conditions => ["roleable_type='<%= role_model_name %>' AND <%= role_model_file_name %>_id=? AND roleable_id=?",roleable_id,<%= role_model_file_name %>_id])
-        errors.add_to_base("A <%= role_model_file_name %> cannot belong to a <%= role_model_file_name %> which belongs to it.")
+  def belongs_to_itself_through_other?(original_roleable_id, current_<%= role_model_file_name %>_id)
+    if self.class.find(:first, :select => "id", :conditions => ["roleable_id=? AND roleable_type='<%= role_model_name %>' AND <%= role_model_file_name %>_id=?",current_<%= role_model_file_name %>_id,original_roleable_id])
+      return true
+    else
+      memberships = self.class.find(:all, :select => "<%= role_model_file_name %>_id", :conditions => ["roleable_id=? AND roleable_type='<%= role_model_name %>'",current_<%= role_model_file_name %>_id])
+      if memberships.any? {|membership| belongs_to_itself_through_other?(original_roleable_id,membership.<%= role_model_file_name %>_id)}
+        return true
       end
     end
+    return false
   end
 end
--- generators/permissible/templates/role_membership_model_spec.rb
+++ generators/permissible/templates/role_membership_model_spec.rb
@@ -3,8 +3,25 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe "<%= role_membership_model_name %>" do
   
   describe "validations" do
+    before(:all) do
+      @<%= role_model_file_name.pluralize %> = []
+      @<%= role_model_file_name.pluralize %>[0] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>0")
+      @<%= role_model_file_name.pluralize %>[1] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>1")
+      @<%= role_model_file_name.pluralize %>[2] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>2")
+      @<%= role_model_file_name.pluralize %>[3] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>3")
+      @<%= role_model_file_name.pluralize %>[4] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>4")
+      @<%= role_model_file_name.pluralize %>[5] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>5")
+      @<%= role_model_file_name.pluralize %>[6] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>6")
+      @<%= role_model_file_name.pluralize %>[7] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>7")
+      @<%= role_model_file_name.pluralize %>[8] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>8")
+      @<%= role_model_file_name.pluralize %>[9] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>9")
+      @<%= role_model_file_name.pluralize %>[10] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>10")
+      @<%= role_model_file_name.pluralize %>[11] = <%= role_model_name %>.new(:name => "<%= role_model_file_name %>11")
+      @<%= role_model_file_name.pluralize %>.each {|<%= role_model_file_name %>| <%= role_model_file_name %>.save!}
+    end
+    
     before(:each) do
-      @membership = <%= role_membership_model_name %>.new(:roleable_id => 1, :roleable_type => "<%= role_model_name %>", :<%= role_model_file_name %>_id => 2)
+      @membership = <%= role_membership_model_name %>.new(:roleable_id => @<%= role_model_file_name.pluralize %>[0].id, :roleable_type => "<%= role_model_name %>", :<%= role_model_file_name %>_id => @<%= role_model_file_name.pluralize %>[1].id)
     end
     
     it "should be valid" do
@@ -54,17 +71,30 @@ describe "<%= role_membership_model_name %>" do
     end
     
     it "should not allow a <%= role_model_file_name %> to belong to itself" do
-      @membership.<%= role_model_file_name %>_id = 1
+      @membership.<%= role_model_file_name %>_id = @<%= role_model_file_name.pluralize %>[0].id
       @membership.should_not be_valid
     end
     
-    # <%= role_model_file_name.pluralize %> cannot belong to each other in a cycle
-    it "should not a allow a <%= role_model_file_name %> to belong to a <%= role_model_file_name %> which belongs to it" do
-      @membership.save!
-      @membership2 = <%= role_membership_model_name %>.new(:roleable_id => 2, :roleable_type => "<%= role_model_name %>", :<%= role_model_file_name %>_id => 1)
-      @membership2.should_not be_valid
-      @membership2.errors.full_messages.should include("A <%= role_model_file_name %> cannot belong to a <%= role_model_file_name %> which belongs to it.")
-      @membership.destroy
+    # <%= role_model_file_name.pluralize %> cannot belong to each other in a loop
+    it "should not a allow a <%= role_model_file_name %> to belong to a <%= role_model_file_name %> which belongs to it in a loop" do
+      @<%= role_model_file_name.pluralize %>[0].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[1]
+      @<%= role_model_file_name.pluralize %>[1].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[2]
+      @<%= role_model_file_name.pluralize %>[2].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[3]
+      @<%= role_model_file_name.pluralize %>[2].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[4]
+      @<%= role_model_file_name.pluralize %>[2].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[5]
+      @<%= role_model_file_name.pluralize %>[3].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[6]
+      @<%= role_model_file_name.pluralize %>[1].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[7]
+      @<%= role_model_file_name.pluralize %>[3].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[8]
+      @<%= role_model_file_name.pluralize %>[4].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[9]
+      @<%= role_model_file_name.pluralize %>[4].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[10]
+      @<%= role_model_file_name.pluralize %>[5].<%= role_model_file_name.pluralize %> << @<%= role_model_file_name.pluralize %>[11]
+      @membership3 = <%= role_membership_model_name %>.new(:roleable_id => @<%= role_model_file_name.pluralize %>[11].id, :roleable_type => "<%= role_model_name %>", :<%= role_model_file_name %>_id => @<%= role_model_file_name.pluralize %>[0].id)
+      @membership3.should_not be_valid
+      @membership3.errors.full_messages.should include("A <%= role_model_file_name %> cannot belong to a <%= role_model_file_name %> which belongs to it.")
+    end
+    
+    after(:all) do
+      @<%= role_model_file_name.pluralize %>.each {|<%= role_model_file_name %>| <%= role_model_file_name %>.destroy}
     end
   end
 end
